9c0d1cf95435f6ac07fbf5beaa9ceefacd13b0e0
dev13n <devpn02@gmail.com>
2024-12-17 22:57:25 +0530

    Added comments

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import fitz  # type: ignore # PyMuPDF
 doc = fitz.open("read_from.pdf")
 
 # Iterate through the pages and extract text
-for page_num in range(doc.page_count):
-    page = doc.load_page(page_num)
+for page_num in range(doc.page_count): # loop to iterate through all the pages of the PDF.
+    page = doc.load_page(page_num) # loads a specific page from the PDF document
     text = page.get_text("text")  # Extract text from the page
     print(text)
